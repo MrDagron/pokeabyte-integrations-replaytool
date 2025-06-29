6c818f78b17da56828b393480e00acebf12b58d3
Andrew <andrewnipp@gmail.com>
2025-06-29 07:56:08 -0700

    Reimplemented a few missing methods from the original project for SaveStateService.cs Created SerializationHelper.cs and reimplemented the JSON serialization/deserialization methods from the original project

--- PokeAByte.Integrations.ReplayTool/Logic/SaveStateService.cs
+++ PokeAByte.Integrations.ReplayTool/Logic/SaveStateService.cs
@@ -1,16 +1,76 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Timers;
 using BizHawk.Common;
 using PokeAByte.Integrations.ReplayTool.Models;
 
 namespace PokeAByte.Integrations.ReplayTool.Logic;
 //https://github.com/endel/FossilDelta/
+/*
+ * TODO:
+ * GetStateData (*)
+ * LoadSaveState (*)
+ * AddSaveRange (*)
+ * UpdateSave
+ * DeleteSave
+ *
+ * (*) Need to think about if I want to include these here since they require the MainForm or if I want to
+ * further decouple this service from the GUI layer
+ */
 public class SaveStateService
 {
     private SaveStateModel? _saveStateModel;
     private ConcurrentQueue<SaveState> _saveStateQueue = new();
     private byte[] _lastState = [];
     private int _currentSize = 0;
+    private Timer _queueTimer;
+    private bool _isSaving = false;
+    public SaveStateService()
+    {
+        InitializeQueueTimer();
+    }
+    private void InitializeQueueTimer()
+    {
+        _queueTimer = new Timer();
+        _queueTimer.Interval = 16;
+        _queueTimer.Elapsed += HandleSaveStateQueue;
+        _queueTimer.AutoReset = true;
+        _queueTimer.Start();
+    }
+    private void HandleSaveStateQueue(object sender, ElapsedEventArgs elapsedEventArgs)
+    {
+        if (_saveStateQueue.Count == 0 || 
+            _lastState.Length == 0 ||
+            _saveStateModel is null)
+        {
+            //Todo: handle this
+            return;
+        }
+
+        if (!_saveStateQueue.TryDequeue(out var saveState))
+        {
+            //Todo: handle this
+            return;
+        }
+        
+        var stateBytes = saveState.FullState;
+        //get the delta between the current state and the last state
+        var delta = Fossil.Delta.Create(_lastState, stateBytes);
+        //Todo: figure out how we should handle the error
+        if (delta is null || delta.Length == 0)
+        {
+            Log.Error(nameof(SaveStateService), 
+                "Failed to get delta between states"); 
+            return;
+        }
+        saveState.StateDelta = delta;
+        //clear the full state to reduce memory usage
+        saveState.FullState = [];
+        _saveStateModel.SaveStates.Add(saveState);
+        //Update the last state
+        _lastState = stateBytes;
+    }
     
     public void SaveState(int frame, 
         byte[] state, 
@@ -41,40 +101,6 @@ public class SaveStateService
         });
         _currentSize += 1;
     }
-
-    //Todo: create a timer to execute this method
-    public void HandleSaveStateQueue()
-    {
-        if (_saveStateQueue.Count == 0 || 
-            _lastState.Length == 0 ||
-            _saveStateModel is null)
-        {
-            //Todo: handle this
-            return;
-        }
-
-        if (!_saveStateQueue.TryDequeue(out var saveState))
-        {
-            //Todo: handle this
-            return;
-        }
-        
-        var stateBytes = saveState.FullState;
-        //get the delta between the current state and the last state
-        var delta = Fossil.Delta.Create(_lastState, stateBytes);
-        //Todo: figure out how we should handle the error
-        if (delta is null || delta.Length == 0)
-        {
-            Log.Error(nameof(SaveStateService), "Failed to get delta between states"); 
-            return;
-        }
-        saveState.StateDelta = delta;
-        //clear the full state to reduce memory usage
-        saveState.FullState = [];
-        _saveStateModel.SaveStates.Add(saveState);
-        //Update the last state
-        _lastState = stateBytes;
-    }
     
     public void ReconstructSaveStates()
     {
@@ -89,14 +115,16 @@ public class SaveStateService
             if (delta.Length == 0)
             {
                 //Todo: figure out how we should handle this
-                Log.Error(nameof(SaveStateService), "Failed to get delta between states");
+                Log.Error(nameof(SaveStateService), 
+                    "Failed to get delta between states");
                 return;
             }
             var reconstructedState = Fossil.Delta.Apply(lastState, delta);
             if (reconstructedState is null || reconstructedState.Length == 0)
             {
                 //Todo: figure out how we should handle this
-                Log.Error(nameof(SaveStateService), "Failed to reconstruct state");
+                Log.Error(nameof(SaveStateService), 
+                    "Failed to reconstruct state");
                 return;
             }
             saveState.FullState = reconstructedState;
@@ -104,4 +132,71 @@ public class SaveStateService
         }
         _saveStateModel.HasBeenReconstructed = true;
     }
+
+    public void SaveToFile(string path)
+    {
+        if (_isSaving)
+        {
+            return;
+        }
+        _queueTimer.Stop();
+        _isSaving = true;
+
+        try
+        {
+            var serializeMessage = SerializationHelper.SerializeJsonToFile(_saveStateModel, path);
+            if (!string.IsNullOrEmpty(serializeMessage))
+            {
+                Log.Error(nameof(SaveStateService), 
+                    "Failed to save to file: {serializeMessage}", 
+                    serializeMessage);
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error(nameof(SaveStateService), 
+                "Failed to save to file: {e}",
+                e);
+        }
+        
+        _isSaving = false;
+        _queueTimer.Start();
+    }
+
+    public void LoadFromFile(string path)
+    {
+        if (_isSaving)
+        {
+            return;
+        }
+        _queueTimer.Stop();
+        try
+        {
+            var deserialized = SerializationHelper.DeserializeJsonFromFile<SaveStateModel>(path);
+            if (!string.IsNullOrEmpty(deserialized.Message))
+            {
+                Log.Error(nameof(SaveStateService), 
+                    "Failed to load from file: {deserializeMessage.Item1}", 
+                    deserialized.Message);
+            }
+
+            else if (deserialized.Data is null)
+            {
+                Log.Error(nameof(SaveStateService), 
+                    "Failed to load from file: deserialized.Data is null");
+            }
+            else
+            {
+                _saveStateModel = deserialized.Data;
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error(nameof(SaveStateService), 
+                "Failed to load from file: {e}", e);
+        }
+        _queueTimer.Start();
+    }
+    
+ 
 }
---
+++ PokeAByte.Integrations.ReplayTool/Logic/SerializationHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PokeAByte.Integrations.ReplayTool.Logic;
+
+/*
+ * Todo:
+ * Serialize (this is a XML serializer, I believe this was for the events file? Might not be required)
+ * Deserialize (this is a XML serializer, same idea as the Serialize not sure we need it) 
+ * SerializeJson (instead of saving to a file, this just returns byte data. Not sure we need it?)
+ */
+
+public static class SerializationHelper
+{
+    //Returns a string that is empty if the serialization was successful, otherwise it returns the exception message.
+    public static string SerializeJsonToFile<T>(T toSerialize, string path)
+    {
+        if (toSerialize is null)
+            return "Failed to serialize to file: toSerialize is null";
+        try
+        {
+            using var writer = new StreamWriter(path);
+            using var jsonWriter = new JsonTextWriter(writer);
+            var serializer = new JsonSerializer();
+            serializer.Serialize(jsonWriter, toSerialize);
+            jsonWriter.Flush();
+            return "";
+        }
+        catch (Exception e)
+        {
+            return "Failed to serialize to file: " + e;
+        }
+    }
+    //Returns a tuple of string and T, where the string returns the exception message if there is one otherwise it is 
+    //empty.
+    public static (string Message, T? Data) DeserializeJsonFromFile<T> (string path)
+    {
+        try
+        {
+            using var reader = new StreamReader(path);
+            using var jsonReader = new JsonTextReader(reader);
+            var serializer = new JsonSerializer();
+            return ("", serializer.Deserialize<T>(jsonReader));
+        }
+        catch (Exception e)
+        {
+            return ("Failed to deserialize from file: " + e,default);
+        }
+    }
+}
